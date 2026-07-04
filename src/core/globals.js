d8cccf7392b898d71f1dbbccbbf912b8cb121e21
Hasan Rizvee <rizveehasan@gmail.com>
2026-07-05 03:45:33 +0600

    fix: align CLI fallback version with package

--- src/core/globals.js
+++ src/core/globals.js
@@ -10,7 +10,7 @@ const __dirname = dirname(__filename);
 // So resolving '..' will give the project root directory
 export const sourceRoot = resolve(__dirname, '..');
 
-let pkgVersion = '3.0.2';
+let pkgVersion = '3.5.0';
 try {
   const pkgData = JSON.parse(readFileSync(resolve(sourceRoot, 'package.json'), 'utf8'));
   pkgVersion = pkgData.version;
